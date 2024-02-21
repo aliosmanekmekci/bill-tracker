f20d28c8adabb02cc1de7734424cf584dc3f88a6
ali osman ekmekci <aliosmanekmekci@gmail.com>
2024-02-21 14:01:16 +0300

    refactor: define initial project structure and implement Chakra context provider

---
+++ src/context/AppProviders.jsx
@@ -0,0 +1,9 @@
+import { ChakraProvider } from "@chakra-ui/react";
+
+export function AppProviders() {
+  return (
+    <ChakraProvider>
+      <div>Work In Progress</div>
+    </ChakraProvider>
+  );
+}
