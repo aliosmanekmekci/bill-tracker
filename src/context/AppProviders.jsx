8c59094cf203288e73ec38d43662a0b27cd9eff5
ali osman ekmekci <aliosmanekmekci@gmail.com>
2024-02-21 14:13:05 +0300

    chore: remove chakra from project

--- src/context/AppProviders.jsx
+++ src/context/AppProviders.jsx
@@ -1,10 +1,5 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import { Router } from "./Router";
 
 export function AppProviders() {
-  return (
-    <ChakraProvider>
-      <Router />
-    </ChakraProvider>
-  );
+  return <Router />;
 }
