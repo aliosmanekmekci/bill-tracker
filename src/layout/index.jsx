f9be922418497815d438b707069f3a79bcb914c9
ali osman ekmekci <aliosmanekmekci@gmail.com>
2024-02-22 15:32:32 +0300

    search bar added

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,5 +1,6 @@
-import { AppShell } from "@mantine/core";
+import { AppShell, MultiSelect, Title } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import "./index.scss";
 
 export function AppLayout(props) {
   const [opened, { toggle }] = useDisclosure();
@@ -15,7 +16,16 @@ export function AppLayout(props) {
       padding="md"
     >
       <AppShell.Header>
-        <div>Header WIP</div>
+        <div className="header">
+          <Title order={3} m={10}>
+            Fatura Takip Sistemi
+          </Title>
+          <MultiSelect
+            placeholder="Bul..."
+            data={["Borç", "Gecikme", "Alacak", "Bakiye"]}
+            searchable
+          />
+        </div>
       </AppShell.Header>
 
       <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
