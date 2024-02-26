ad506070c3eea9293cdbd422e11b07807241aca3
ali osman ekmekci <aliosmanekmekci@gmail.com>
2024-02-26 15:43:40 +0300

    NavLink Icons added

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,6 +1,8 @@
-import { AppShell, MultiSelect, Title } from "@mantine/core";
+import { AppShell, MultiSelect, NavLink, Title } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { IconHome2 } from "@tabler/icons-react";
 import "./index.scss";
+import { IconAlignBoxBottomCenterFilled } from "@tabler/icons-react";
 
 export function AppLayout(props) {
   const [opened, { toggle }] = useDisclosure();
@@ -28,7 +30,20 @@ export function AppLayout(props) {
         </div>
       </AppShell.Header>
 
-      <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+      <AppShell.Navbar p="md">
+        <NavLink
+          href="#required-for-focus"
+          label="Ana Sayfa"
+          leftSection={<IconHome2 size="1rem" stroke={1.5} />}
+        />
+        <NavLink
+          href="#required-for-focus"
+          label="Detaylar"
+          leftSection={
+            <IconAlignBoxBottomCenterFilled size="1rem" stroke={1.5} />
+          }
+        />
+      </AppShell.Navbar>
 
       <AppShell.Main>{props.children}</AppShell.Main>
     </AppShell>
