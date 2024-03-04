a8ca4c583310f77182cd67b9b8380e9f4ad4111c
ali osman ekmekci <aliosmanekmekci@gmail.com>
2024-03-04 18:37:39 +0300

    Details component changed with News API

--- src/routes/Details/index.jsx
+++ src/routes/Details/index.jsx
@@ -1,21 +1,47 @@
-import { Accordion } from "@mantine/core";
+import { Accordion, Loader } from "@mantine/core";
 import { useEffect, useState } from "react";
-import { fetchUpcomingBills } from "../../utils/request";
 
 export default function Details() {
-  const [bills, setBills] = useState([]);
+  const [newsItems, setNewsItems] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetchUpcomingBills().then((data) => setBills(data));
+    fetch(
+      "https://newsdata.io/api/1/news?apikey=pub_39398456c4ae1777ca05a2a6dc24fd9e30280&q=pizza"
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setNewsItems(data.results); // Access the correct field from the API response
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
   }, []);
-  
-  // See details data above
-  const items = bills.map((bill) => (
-    <Accordion.Item key={bill.id} value={bill.type}>
-      <Accordion.Control>{bill.type}</Accordion.Control>
-      <Accordion.Panel>₺{bill.dept}</Accordion.Panel>
-    </Accordion.Item>
-  ));
+
+  if (isLoading) {
+    return <Loader color="cyan" size="sm" />;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  const items =
+    newsItems &&
+    newsItems.map((newsItem) => (
+      <Accordion.Item key={newsItem.article_id} value={newsItem.title}>
+        <Accordion.Control>{newsItem.title}</Accordion.Control>
+        <Accordion.Panel>{newsItem.description}</Accordion.Panel>
+      </Accordion.Item>
+    ));
 
   return (
     <Accordion variant="contained" chevronPosition="left" defaultValue="Borç">
